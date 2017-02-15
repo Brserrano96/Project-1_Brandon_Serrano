abe7548d210a8de75f15508e0dc7880559fb0055
Brandon Serrano <brandonserrano1212@yahoo.com>
2017-02-15 10:56:41 -0800

    Initial Commit

---
+++ Project_1_BrandonSerrano/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project_1_BrandonSerrano
+//
+//  Created by BRANDON SERRANO on 2/15/17.
+//  Copyright © 2017 Trendsverse. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
